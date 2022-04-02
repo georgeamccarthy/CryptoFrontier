abc97106ceb30786f6fd26d89f35184a030f2699
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2022-04-02 23:37:39 +0100

    Allow selection of starting year

--- crypto_frontier/app.py
+++ crypto_frontier/app.py
@@ -3,8 +3,15 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import os
+
+current_dir = os.path.dirname(__file__)
+data_path = os.path.join(current_dir, './data/coin_data_2019.csv')
 
 st.title("CryptoFrontier")
+
+years = ["2019", "2021"]
+st.selectbox(label="Use Crypto data since which year", options=years)
 
 all_coin_codes = efficient_frontier.get_coin_codes()
 #coin_codes = coin_codes[:10]
@@ -22,7 +29,7 @@ if st.button("Submit"):
     selected_coins = list(buttons.keys())
     coin_percentages = list(buttons.values())
 
-    df = pd.read_csv("./data/coin_data_2021.csv")[selected_coins]
+    df = pd.read_csv(data_path)[selected_coins]
     users_risk, users_return = efficient_frontier.users_point(df, coin_percentages)
     df, risk, returns = efficient_frontier.efficient_frontier(df, n_portfolios)
 
