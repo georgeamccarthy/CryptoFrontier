176004b3d461e28751b5f2c2ac7f37fe3e655eae
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2022-04-02 21:02:22 +0100

    Move slider outside submit

--- crypto_frontier/app.py
+++ crypto_frontier/app.py
@@ -22,6 +22,8 @@ buttons = {}
 for coin_code in coin_codes:
     buttons[coin_code] = st.number_input(coin_code, 0, 100, key=coin_code)
 
+n_portfolios = st.slider('Choose number of generated portfolios', 20, 500, value=200)
+
 if st.button("Submit"):
     coin_codes = list(buttons.keys())
     coin_percentages = list(buttons.values())
@@ -31,7 +33,6 @@ if st.button("Submit"):
     coin_codes = list(np.array(coin_codes)[mask])
     coin_percentages = list(np.array(coin_percentages)[mask])
 
-    n_portfolios = st.slider('Choose number of generated portfolios', 20, 500, value=200)
     df = efficient_frontier.download_data(coin_codes)
     df, risk, returns = efficient_frontier.efficient_frontier(df, n_portfolios)
 
